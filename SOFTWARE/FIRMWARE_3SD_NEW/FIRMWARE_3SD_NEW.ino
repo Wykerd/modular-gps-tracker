1a34323c0456b18e60854c8aa09d4eb22f61ae1f
Daniel Wykerd <danielwykerd@gmail.com>
2019-11-22 21:09:37 +0200

    Initialize git repo from old non git project

---
+++ SOFTWARE/FIRMWARE_3SD_NEW/FIRMWARE_3SD_NEW.ino
@@ -0,0 +1,232 @@
+/*  Modular GSM GPS Tracker Firmaware 3A
+ *  Software by Daniel Wykerd
+ *  
+ *  Features:
+ *  - Auto SMS receive and loading into character arrays
+ *  - Responds to the commands #HELP and #LOCATION
+ *  
+ *  This software is made to be used with REV 1.0 of the core module
+ */
+
+#include <String.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include <AltSoftSerial.h>
+SoftwareSerial sms = SoftwareSerial(2, 3);
+SoftwareSerial sd = SoftwareSerial(11, 12);
+AltSoftSerial ssgps;
+
+TinyGPSPlus gps;
+
+char atResponse[120];
+char notificationBuf[80];
+char senderNum[20];
+char msg[20];
+boolean smsFound = false;
+bool record = true;
+
+const char format = ".GPX";
+
+void setup() {
+  // put your setup code here, to run once:
+  sd.begin(9600);
+  sms.begin(9600);
+  ssgps.begin(9600);
+  Serial.begin(115200);
+  while(!networkStatus()); //Do nothing while there is no network
+  sendAT("AT+CMGF=1");
+  sendAT("AT+CNMI=2,1");
+  deleteSMS();
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW); 
+  Serial.println(F("Boot Successful"));
+  record = false;
+  while (sms.available() > 0) sms.read(); //Clear the buffer before starting to prevent data loss in first sms
+}
+
+void deleteSMS(){
+  sendAT("AT+CMGD=1,4");
+}
+
+void startSMS(){
+  sms.print("AT+CMGS=\"" + String(senderNum) + "\"\r");
+  delay(100);
+}
+
+void sendSMS(){
+  sms.write(0x1A);
+  sms.write(0x0D);
+  sms.write(0x0A); 
+  Serial.print(F("\nSMS Sent!"));
+}
+
+uint8_t preSec;
+
+void loop() {
+  preSec = gps.time.second();
+  parseGPS();
+  // put your main code here, to run repeatedly:
+  checkForSMS();
+  if (smsFound) {
+    smsFound = false;
+    Serial.print(F("From: "));
+    Serial.print(senderNum);
+    Serial.print(F("\nMessage: "));
+    Serial.print(msg);
+    String sMsg = String(msg);
+    sMsg.toUpperCase();
+    if (msg[0] == '#') {
+      if (sMsg.indexOf('HELP') > 0) {
+        startSMS();
+        sms.print(F("HELP\n"));
+        sms.print(F("Dev Build 3SD"));
+        sms.print(F("\n  #LOCATION - Sends current location"
+                    "\n  #RECORD - Logs location to SD Module"));
+        sendSMS();  
+      }else if (sMsg.indexOf('LOCATION') > 0) {
+        startSMS();
+        if (gps.satellites.value() >= 3) {
+          //Sends location
+          sms.print(F("Location: "));
+          sms.print(gps.location.lat(), 6);
+          sms.print(F(", "));
+          sms.print(gps.location.lng(), 6);
+          //Sends link to Google Maps
+          sms.print(F("\nGoogle maps: "));
+          sms.print(F("google.com/maps/place/"));
+          sms.print(gps.location.lat(), 6);
+          sms.print(F(","));
+          sms.print(gps.location.lng(), 6);
+          //Send amount of satellites for debugging
+          sms.print(F("\nSatellites: "));
+          sms.print(gps.satellites.value());
+        } else {
+          sms.print(F("Acquiring satellites, try later"));
+        }
+        sendSMS();
+      }else if (sMsg.indexOf('RECORD') > 0) {
+        recordCommand();
+      }
+    }
+  }
+  
+  char ftime[20];
+  sprintf(ftime, "%04d-%02d-%02dT%02d:%02d:%02dZ", gps.date.year(), gps.date.month(), gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second());  
+  
+  if (((record)&&(preSec != gps.time.second()))&&(gps.satellites.value() >= 3)) {
+    //Add to log
+    sd.print("<trkpt lat=\"");
+    sd.print(gps.location.lat(), 6);
+    sd.print("\" lon=\"");  
+    sd.print(gps.location.lng(), 6);
+    sd.print("\">\n\r<time>");
+    delay(100);
+    sd.print(ftime);
+    sd.print("</time>\n\r<ele>");
+    sd.print(gps.altitude.meters());
+    delay(100);
+    sd.println("</ele></trkpt>");
+    delay(100); 
+  }
+}
+
+void recordCommand(){
+  record = !record;
+  startSMS();
+  if (record){
+    startGPX();
+    sms.print(F("Started Recording"));
+  }
+  else{
+    endGPX();
+    sms.print(F("Ended Recording"));
+  }
+  sendSMS();
+}
+
+void startGPX(){
+  digitalWrite(13, HIGH);
+  delay(5000);
+  while (gps.satellites.value() < 3) parseGPS();
+  sd.print(gps.date.value());
+  sd.print(gps.time.hour());
+  sd.print(".GPX");
+  delay(200);
+  sd.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+  delay(50);
+  sd.print("<gpx version=\"1.1\" xmlns=\"http://www.topograf");
+  delay(50);
+  sd.print("ix.com/GPX/1/1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-in");
+  delay(50);
+  sd.print("stance\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1");
+  delay(50);
+  sd.println(" http://www.topografix.com/GPX/1/1/gpx.xsd\">");
+  delay(50);
+  sd.println("<trk><trkseg>");
+  delay(100);
+}
+
+void endGPX() {
+  sd.print("</trkseg>\n\r</trk>\n\r</gpx>");
+  delay(1000);
+  digitalWrite(13, LOW);
+}
+
+void parseGPS(){
+  while (ssgps.available() > 0) gps.encode(ssgps.read());
+}
+
+void getSMSDat(){
+  sendAT("AT+CMGR=1");
+  if ((strstr(atResponse, "+CMGR:")) != NULL){ //Make sure the response is correct
+    sscanf(atResponse, "%[^,],\"%[^\"]\"%[^\r\n]\r\n%[^\r\n]", NULL, senderNum, NULL, msg); //PARSE THE DATA
+    deleteSMS(); //CLEAR THE MODEM MEMORY!
+  }
+}
+
+void checkForSMS(){
+  uint8_t i = 0;
+  memset(notificationBuf, '\0', 100);
+  while ((sms.available() != 0) && (i < 255)){
+    notificationBuf[i] = sms.read();
+    Serial.print(notificationBuf[i]);
+    i++; 
+  }
+  delay(100);
+  if ((strstr(notificationBuf, "+CMTI")) != NULL){
+    getSMSDat();
+    smsFound = true;
+  }
+}
+
+boolean networkStatus(){
+  sendAT("AT+CGREG?");
+
+  //parse the response
+  if ((strstr(atResponse, "+CGREG:")) != NULL){ //Make sure the response is correct
+    strtok(atResponse," :,");
+    strtok (NULL," :,");  
+    if ((strstr ((strtok (NULL," :,")),"1")) != NULL) return true; 
+    else return false;
+    
+  } else return false; 
+}
+
+void sendAT(char* atCommand){
+  uint8_t i=0;
+  memset(atResponse, '\0', 100); //Clear the char array
+  while( sms.available() > 0) sms.read();
+  sms.println(atCommand);     //Print the at command
+  Serial.print(F("<- "));
+  Serial.println(atCommand);
+  delay(100);
+  unsigned long initialTime = millis();
+  do{
+    if (sms.available() != 0){
+      atResponse[i] = sms.read();
+      i++; 
+    }
+  } while((strstr(atResponse, "OK") == NULL) && (millis() - initialTime)<2000); //Keep searching for response for 2 sec or until "OK" which indicates the end of a AT response
+  Serial.print(F("-> "));Serial.println(atResponse);
+}
+
---
+++ SOFTWARE/SD_GPS_MODULE_REV1/SD_GPS_MODULE_REV1.ino
@@ -0,0 +1,58 @@
+/*
+ * Software fo the Snap|Click|Track SD module rev 1
+ * Software version 1; Created 7 September 2018
+ * 
+ * This code is made for one way data transfer to SD card
+ * 
+ * Usage instructions to interface with other modules:
+ * - Once the module is turnt on it waits for a string of data on sent to SerialSoftware(3,2)
+ * - This string is loaded into the filename buffer. All data sent after this point will be written to the filename path.
+ * - The software only supports writng of data, no data can be retrieved. Thie source code can be modified to send back data though serial.
+ * - Send 64 bytes or less at a time!
+ */
+
+#include <SPI.h>
+#include <SD.h>
+#include <SoftwareSerial.h>
+SoftwareSerial ss = SoftwareSerial(3,2);
+
+File dataFile;
+
+int i = 0;
+
+char filename[25];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //ss.begin(9600);
+  Serial.println(F("BOOTING"));
+  if (!SD.begin()) {
+    Serial.println(F("SD FAILED"));
+    return;
+  }
+  Serial.println(F("DONE"));
+  while (!Serial.available()); //Do nothing
+  while (Serial.available()){  //Read data to filename
+    filename[i] = Serial.read();  
+    Serial.print(filename[i]);
+    i++; 
+    delay(10); //wait for more data
+  }
+
+  Serial.print("\n\rFilename: ");  
+  Serial.println(filename);
+  //Create the file
+  dataFile = SD.open(String(filename), FILE_WRITE);
+  dataFile.close();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dataFile = SD.open(String(filename), FILE_WRITE);
+  while (Serial.available()){
+    dataFile.print((char) Serial.read());
+    Serial.print(".");
+  }
+  dataFile.close();
+}
